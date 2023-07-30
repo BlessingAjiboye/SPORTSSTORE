9629ed83e81cc3edeaa8e60d8ad7743d90e35214
Blessing Ajiboye <blessjiboye@yahoo.com>
2023-07-29 22:40:45 -0400

    Added Redirects file

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -13,7 +13,11 @@ const AUTH_API = 'https://sportsstoren.onrender.com/api/auth/';
 
 const httpOptions = {
 
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  })
 
 };
 
